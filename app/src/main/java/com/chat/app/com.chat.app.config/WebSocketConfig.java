734cc4d2523401909ecb56f680ff2e3b61460e55
Shaivi Sahu <sahushaivi00@gmail.com>
2026-03-21 19:42:05 +0530

    Initial commit

---
+++ app/src/main/java/com/chat/app/com.chat.app.config/WebSocketConfig.java
@@ -0,0 +1,6 @@
+package com.chat.app.com.chat.app.config;
+
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+}
