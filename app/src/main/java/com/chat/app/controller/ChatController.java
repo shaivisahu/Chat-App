a6fe6a123e4f0a00b23b30eea37180099bf8f4fd
Shaivi Sahu <sahushaivi00@gmail.com>
2026-03-24 23:03:24 +0530

    update

--- app/src/main/java/com/chat/app/controller/ChatController.java
+++ app/src/main/java/com/chat/app/controller/ChatController.java
@@ -1,11 +1,25 @@
 package com.chat.app.controller;
 
 import com.chat.app.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ChatController {
+
+    // /app/sendMassage
+
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/messages")
     public ChatMessage sendMassage(ChatMessage message){
         return message;
     }
+
+    @GetMapping("chat")
+    public String chat(){
+        return "chat";
+    }
+
 }
