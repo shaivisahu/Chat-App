2ddb810d8929c2af98b277f7de854ba7c5a31286
Shaivi Sahu <sahushaivi00@gmail.com>
2026-03-27 23:26:59 +0530

    update

--- app/src/main/java/com/chat/app/controller/ChatController.java
+++ app/src/main/java/com/chat/app/controller/ChatController.java
@@ -1,6 +1,7 @@
 package com.chat.app.controller;
 
 import com.chat.app.model.ChatMessage;
+
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
