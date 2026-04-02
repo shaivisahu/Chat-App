965cb409921aa2f2ac8f3ca22bfb3067ad0e15f2
Shaivi Sahu <sahushaivi00@gmail.com>
2026-04-02 23:47:13 +0530

    fest:ui with the help of themleaf

--- app/src/main/java/com/chat/app/controller/ChatController.java
+++ app/src/main/java/com/chat/app/controller/ChatController.java
@@ -1,7 +1,6 @@
 package com.chat.app.controller;
 
 import com.chat.app.model.ChatMessage;
-
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
